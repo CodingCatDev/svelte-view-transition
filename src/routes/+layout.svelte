ad87275c7ac5e69062706166d34873891e28073c
Alex Patterson <alex@codingcat.dev>
2023-12-04 12:32:40 -0500

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.pcss';
+	const links = ['/home', '/about', '/blog', '/contact'];
+</script>
+
+<div class="flex gap-1">
+	{#each links as link}
+		<a class="underline" href={link}>{link.substring(1)}</a>
+	{/each}
+</div>
+<slot />
